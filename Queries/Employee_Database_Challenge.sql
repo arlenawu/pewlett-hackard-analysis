8c5cac761cdfab20ebd2f76386d0893cef8799fb
arlenawu <arlenawu@gmail.com>
2021-06-10 19:34:33 -0500

    adding deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -3,9 +3,9 @@ SELECT * FROM titles;
 
 -- DELIVERABLE 1 ------------------------------------------------
 
-DROP TABLE retirement_titles;
+DROP TABLE IF EXISTS retirement_titles;
 
--- Create retirement_titles table
+-- Identify employees eligible for retirement
 -- all employees born between 1952 and 1955
 SELECT e.emp_no,
 e.first_name,
@@ -23,7 +23,7 @@ ORDER BY e.emp_no, t.from_date DESC;
 SELECT * FROM retirement_titles;
 
 ----------------------------------------------------------------
-DROP TABLE unique_titles;
+DROP TABLE IF EXISTS unique_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -38,9 +38,9 @@ SELECT * FROM unique_titles;
 
 ----------------------------------------------------------------
 
-DROP TABLE retiring_titles;
+DROP TABLE IF EXISTS retiring_titles;
 
--- Get retiring employees count by job title
+-- Count retiring employees by job title
 SELECT COUNT(emp_no), title
 INTO retiring_titles
 FROM unique_titles
@@ -51,4 +51,24 @@ SELECT * FROM retiring_titles;
 
 
 -- DELIVERABLE 2 ------------------------------------------------
+DROP TABLE IF EXISTS mentorship_eligibilty;
 
+-- Identify mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
